49c53e7b5dcef7415e9b02803fa3aa7de1947a89
Igor <dexuser30@gmail.com>
2019-12-18 01:06:34 +0200

    [+] Сonfigured router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,40 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { CaseStudiesComponent } from './case-studies/case-studies.component';
+import { ServicesComponent } from './services/services.component';
+import { ExpertiseComponent } from './expertise/expertise.component';
+import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '',
+    component: HomeComponent,
+    data: { title: 'Test case' }
+  },
+  { path: 'case_studies',
+    component: CaseStudiesComponent,
+    data: { title: 'Case studies page' }
+  },
+  { path: 'services',
+    component: ServicesComponent,
+    data: { title: 'Services page' }
+  },
+  { path: 'expertise',
+    component: ExpertiseComponent,
+    data: { title: 'Expertise page' }
+  },
+  { path: 'about',
+    component: AboutComponent,
+    data: { title: 'About page' }
+  },
+  { path: 'contacts',
+    component: ContactsComponent,
+    data: { title: 'Contacts page' }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CaseStudiesComponent } from './case-studies/case-studies.component';
+import { ServicesComponent } from './services/services.component';
+import { ExpertiseComponent } from './expertise/expertise.component';
+import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CaseStudiesComponent,
+    ServicesComponent,
+    ExpertiseComponent,
+    AboutComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,

42e4a56893a69b5b98ed6868ac2d1bceeb4c0027
Igor <dexuser30@gmail.com>
2019-12-19 14:38:51 +0200

    [+] Popup main menu for mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-case';
+
+  classApplied = false;
+
+  toggleClass() {
+    this.classApplied = !this.classApplied;
+  }
 }
